c4d82a74d6432fbfc78d26ea5346fe9ef3969352
Rachel Bonny <racloubon@gmail.com>
2019-04-12 16:35:13 +0200

    refactor: component as function

--- play-me/src/components/AudioButtons.js
+++ play-me/src/components/AudioButtons.js
@@ -1,30 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
-import { setSelectedSong } from '../redux/actions.js'
+import { setSelectedSong } from '../redux/actions'
 
-class AudioButtons extends Component {
-
-  render() {
-    const { audio, searchResults, trackId, setSelectedSong } = this.props;
-    const trackIndex = searchResults.findIndex(track => track.trackId === trackId);
-    let prevTrack = null;
-    let nextTrack = null;
-    if (trackIndex > 0) prevTrack = searchResults[trackIndex - 1]
-    if (trackIndex <  searchResults.length - 1) nextTrack = searchResults[trackIndex + 1]
-    return (
-      <div className="audioButtons">
-        {prevTrack ? <Link
+const AudioButtons = ({ audio, searchResults, trackId, setSelectedSong }) => {
+  const trackIndex = searchResults.findIndex(track => track.trackId === trackId);
+  let prevTrack = null;
+  let nextTrack = null;
+  if (trackIndex > 0) prevTrack = searchResults[trackIndex - 1]
+  if (trackIndex <  searchResults.length - 1) nextTrack = searchResults[trackIndex + 1]
+  return (
+    <div className="audioButtons">
+      {prevTrack
+        ? <Link
           to={`/song/${prevTrack.trackId}`}
-          onClick={() => setSelectedSong(prevTrack)}><button className="button skip">Prev</button></Link> : ''}
-        <button className="button play" onClick={() => audio.play()}></button>
-        <button className="button pause" onClick={() => audio.pause()}></button>
-        {nextTrack ? <Link
+          onClick={() => setSelectedSong(prevTrack)}>
+            <button className="button skip">
+              Prev
+            </button>
+          </Link>
+        : ''
+      }
+        <button
+          className="button play"
+          onClick={() => audio.play()}>
+        </button>
+        <button
+          className="button pause"
+          onClick={() => audio.pause()}>
+        </button>
+      {nextTrack
+        ? <Link
           to={`/song/${nextTrack.trackId}`}
-          onClick={() => setSelectedSong(nextTrack)}><button className="button skip">Next</button></Link> : ''}
-      </div>
-    )
-  }
+          onClick={() => setSelectedSong(nextTrack)}>
+            <button className="button skip">
+              Next
+            </button>
+          </Link>
+        : ''
+      }
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => ({
