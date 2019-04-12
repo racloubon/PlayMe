90e2f15bbe4dcccde8308d9ee5cd2e605b9c3666
Rachel Bonny <racloubon@gmail.com>
2019-04-12 16:40:33 +0200

    refactor: removed unnecessary Component import

--- play-me/src/components/SearchResults.js
+++ play-me/src/components/SearchResults.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SearchResultsItem from './SearchResultsItem.js';
 import Dropdown from 'react-bootstrap/Dropdown';
 import CardGroup from 'react-bootstrap/CardGroup';
