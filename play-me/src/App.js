590ca68dde98ae11b1c64e52e4347110207df8cf
Rachel Bonny <racloubon@gmail.com>
2019-04-12 16:07:03 +0200

    refactor: component as function

--- play-me/src/App.js
+++ play-me/src/App.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-import SearchPage from './components/SearchPage.js';
-import SongPage from './components/SongPage.js';
+import SearchPage from './components/SearchPage';
+import SongPage from './components/SongPage';
 
-class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Play Me</h1>
-        <Router>
-          <Route path="/" exact component={SearchPage} />
-          <Route path="/song/:id" component={SongPage} />
-        </Router>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Play Me</h1>
+      <Router>
+        <Route
+          path="/"
+          exact component={SearchPage} />
+        <Route
+          path="/song/:id"
+          component={SongPage} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
