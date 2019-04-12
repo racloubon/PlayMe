20e1c1bd8e4a83c4853cfcc614613b5e90bb30ed
Rachel Bonny <racloubon@gmail.com>
2019-04-12 12:40:25 +0200

    fix: sort by genre

--- play-me/src/redux/reducers.js
+++ play-me/src/redux/reducers.js
@@ -17,20 +17,25 @@ const initialState = {
 }
 
 const searchResults = (state = initialState.searchResults, action) => {
-  console.log(action, 'action')
   switch (action.type) {
     case SET_SEARCH_RESULTS:
       return action.searchResults.results
-    case SORT_SEARCH_RESULTS_BY_GENRE_ASCENDING:
-      return [...state].sort((a,b) => a.primaryGenreName.toLowerCase() - b.primaryGenreName.toLowerCase())
-    case SORT_SEARCH_RESULTS_BY_GENRE_DESCENDING:
-      return [...state].sort((a,b) => a.primaryGenreName.toLowerCase() - b.primaryGenreName.toLowerCase()).reverse()
     case SORT_SEARCH_RESULTS_BY_PRICE_ASCENDING:
-    console.log('price ascending')
       return [...state].sort((a,b) => a.trackPrice*100 - b.trackPrice*100)
     case SORT_SEARCH_RESULTS_BY_PRICE_DESCENDING:
-    console.log('price descending')
       return [...state].sort((a,b) => a.trackPrice*100 - b.trackPrice*100).reverse()
+    case SORT_SEARCH_RESULTS_BY_GENRE_ASCENDING:
+      return [...state].sort((a,b) => {
+        const genreA = a.primaryGenreName.toLowerCase();
+        const genreB = b.primaryGenreName.toLowerCase()
+        return (genreA < genreB) ? -1 : (genreA > genreB) ? 1 : 0;
+      })
+    case SORT_SEARCH_RESULTS_BY_GENRE_DESCENDING:
+    return [...state].sort((a,b) => {
+      const genreA = a.primaryGenreName.toLowerCase();
+      const genreB = b.primaryGenreName.toLowerCase()
+      return (genreA > genreB) ? -1 : (genreA < genreB) ? 1 : 0;
+    })
     case SORT_SEARCH_RESULTS_BY_TRACKLENGTH_ASCENDING:
       return [...state].sort((a,b) => a.trackTimeMillis - b.trackTimeMillis)
     case SORT_SEARCH_RESULTS_BY_TRACKLENGTH_DESCENDING:
