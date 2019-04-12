2c8de4e52fd1ff94faea17f5bbee495c442aa531
Rachel Bonny <racloubon@gmail.com>
2019-04-12 16:25:51 +0200

    refactor: component as function

--- play-me/src/components/SearchResults.js
+++ play-me/src/components/SearchResults.js
@@ -1,41 +1,44 @@
 import React, { Component } from 'react';
 import SearchResultsItem from './SearchResultsItem.js';
-import Dropdown from 'react-bootstrap/Dropdown'
-import CardGroup from 'react-bootstrap/CardGroup'
+import Dropdown from 'react-bootstrap/Dropdown';
+import CardGroup from 'react-bootstrap/CardGroup';
 
-class SearchResults extends Component {
-
-  render() {
-    const { data, sort } = this.props;
-    return (
-      <div>
-        <Dropdown>
-          <Dropdown.Toggle variant="dark" id="dropdown-basic">
-            Sort Results By
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item
-              onClick={() => sort('priceAscending')}>Price (Lowest first)</Dropdown.Item>
-            <Dropdown.Item
-              onClick={() => sort('priceDescending')}>Price (Highest first)</Dropdown.Item>
-            <Dropdown.Item
-              onClick={() => sort('genreAscending')}>Genre (A-Z)</Dropdown.Item>
-            <Dropdown.Item
-              onClick={() => sort('genreDescending')}>Genre (Z-A)</Dropdown.Item>
-            <Dropdown.Item
-              onClick={() => sort('tracklengthAscending')}>Tracklength (Shortest first)</Dropdown.Item>
-            <Dropdown.Item
-              onClick={() => sort('tracklengthDescending')}>Tracklength (Longest first)</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-          <CardGroup>
-          <div className="searchResults">
-            {data.map((song, index) => <SearchResultsItem key={index} song={song}/>)}
-          </div>
-          </CardGroup>
-      </div>
-    );
-  }
+const SearchResults = ({ data, sort }) => {
+  return (
+    <div>
+      {
+        data.length
+          ? (<Dropdown>
+            <Dropdown.Toggle variant="dark" id="dropdown-basic">
+              Sort Results By
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                onClick={() => sort('priceAscending')}>Price (Lowest first)</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => sort('priceDescending')}>Price (Highest first)</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => sort('genreAscending')}>Genre (A-Z)</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => sort('genreDescending')}>Genre (Z-A)</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => sort('tracklengthAscending')}>Tracklength (Shortest first)</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => sort('tracklengthDescending')}>Tracklength (Longest first)</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>)
+          : ''
+      }
+      <CardGroup>
+        <div className="searchResults">
+          {data.map(song => <SearchResultsItem
+            key={song.trackId}
+            song={song}/>)}
+        </div>
+      </CardGroup>
+    </div>
+  );
 }
+
 
 export default SearchResults;
